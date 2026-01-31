86c9360cd3d20634157df01b4d6d22ee4ec4390c
Vijaya Nandini <vijayanandinimusale@gmail.com>
2026-01-31 19:35:44 +0530

    Configuring init_setup.sh file

---
+++ init_setup.sh
@@ -0,0 +1,15 @@
+echo [$(date)] : "Start"
+
+echo [$(date)] : "Creating virtual environment with python 3.6" 
+
+python -m venv myenv
+
+echo [$(date)] : "Activating the virtual environment"
+
+myenv/\scripts\activate
+
+echo [$(date)] : "installing the dev requirements"
+
+pip install -r requirement.txt 
+
+echo [$(data)] : "end"
